b15fc14814ff2ebb9eda96c0c3ad85ee2bc85851
Qzphs <86579079+Qzphs@users.noreply.github.com>
2023-05-25 12:03:23 +1200

    Allow minigames to be doubled

--- src/Minigame.java
+++ src/Minigame.java
@@ -1,8 +1,17 @@
 abstract class Minigame {
     protected Contract contract;
     protected int dealer;
+    protected int[][] doubles;
     protected boolean gamePlayed = false;
-    public Minigame(int dealer) { this.dealer = dealer; }
+    public Minigame(int dealer) {
+        this.dealer = dealer;
+        this.doubles = new int[][]{
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+        };
+    }
     abstract public void play();
     abstract public int score(int player);
 }
--- src/NoTricks.java
+++ src/NoTricks.java
@@ -6,16 +6,27 @@ public class NoTricks extends Minigame {
         super(dealer);
         contract = Contract.NOTRICKS;
     }
+    public void makeDouble(int doubler, int doubled) {
+        doubles[doubler][doubled] += 1;
+        doubles[doubled][doubler] += 1;
+    }
     public void play() {
         Random random = new Random();
         for (int ii=0; ii<13; ii++) {
             tricksTaken[random.nextInt(4)] += 1;
         }
     }
+    private int baseScore(int player) {
+        return -2 * tricksTaken[player];
+    }
     public int score(int player) {
         if (!gamePlayed) {
             return 0;
         }
-        return -2 * tricksTaken[player];
+        int finalScore = baseScore(player);
+        for (int opponent=0; opponent<4; opponent++) {
+            finalScore += doubles[player][opponent] * (baseScore(player) - baseScore(opponent));
+        }
+        return finalScore;
     }
 }
